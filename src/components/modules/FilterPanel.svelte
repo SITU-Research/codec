b9628077680f56cf0df42efbb84b3aa49fc39ef6
Bora Erden <berden@situ.nyc>
2022-06-11 12:40:14 +0300

    filter capitalize

--- src/components/modules/FilterPanel.svelte
+++ src/components/modules/FilterPanel.svelte
@@ -1,9 +1,5 @@
 <script>
     import { ui_store, filter_toggles } from "../../stores/store";
-
-    function onCheckboxChange(event, filter, possible_value) {
-        $filter_toggles[filter][possible_value] = event.target.checked;
-    }
 </script>
 
 <div
@@ -37,7 +33,9 @@
                     <span>-</span>
                 </label>
             </form>
-            <span style="color:{value ? 'white' : ''}">{filter}</span>
+            <span class="label_text" style="color:{value ? 'white' : ''}"
+                >{filter}</span
+            >
         </div>
     {/each}
 </div>
@@ -118,5 +116,9 @@
     input:checked + span {
         background: white;
         border-color: white;
+    }
+
+    .label_text {
+        text-transform: capitalize;
     }
 </style>
