0922756e3ef0d920d7abb4787594a9985c987e36
Bora Erden <berden@situ.nyc>
2022-11-03 15:06:31 -0400

    add timelinethree ondestroy

--- src/components/modules/TimelineThree.svelte
+++ src/components/modules/TimelineThree.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
   import { throttle } from "underscore";
 
   import * as THREE from "three";
@@ -85,6 +85,35 @@
     init(el, container);
     animate();
     ro.observe(container);
+  });
+
+  onDestroy(() => {
+    renderer.dispose();
+
+    scene.traverse((object) => {
+      if (!object.isMesh) return;
+
+      object.geometry.dispose();
+
+      if (object.material.isMaterial) {
+        cleanMaterial(object.material);
+      } else {
+        // an array of materials
+        for (const material of object.material) cleanMaterial(material);
+      }
+    });
+
+    const cleanMaterial = (material) => {
+      material.dispose();
+
+      // dispose textures
+      for (const key of Object.keys(material)) {
+        const value = material[key];
+        if (value && typeof value === "object" && "minFilter" in value) {
+          value.dispose();
+        }
+      }
+    };
   });
 
   function init(el, container) {
